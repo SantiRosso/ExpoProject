796fa3d9103a75647ec57bd427db879b28af3fd6
SantiRosso <santiagorosso@live.com.ar>
2023-10-18 16:51:26 -0300

    Header component

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+import Header from "./src/components/Header";
+
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.androidView}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header time={time} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  androidView: {
+    paddingTop: Platform.OS === "android" && 30,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
